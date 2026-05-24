739ba01b0aff8e8dc4ba0e85e72baacd11daffd6
jasonwongkwanho <jasonwongkwanho@gmail.com>
2026-05-25 01:31:08 +0800

    Configure Apps Script backend URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.WS_SHOP_CONFIG = {
   // Paste the deployed Apps Script Web App /exec URL here.
   // Example: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
-  appsScriptWebAppUrl: '',
+  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbwCdnPDACGvdT_-pCqn9mgxctbARgDtKEfdNiP0QXHVa2RJGSlkFgUIRjXn9UEZHb28Xg/exec',
   requestTimeoutMs: 90000
 };
