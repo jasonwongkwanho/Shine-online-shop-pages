dfd55bee06e6bc2f27fab873f52170e153aae535
jasonwongkwanho <jasonwongkwanho@gmail.com>
2026-05-25 02:11:21 +0800

    Point Pages frontend to fixed Apps Script deployment

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.WS_SHOP_CONFIG = {
   // Paste the deployed Apps Script Web App /exec URL here.
   // Example: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
-  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbwCdnPDACGvdT_-pCqn9mgxctbARgDtKEfdNiP0QXHVa2RJGSlkFgUIRjXn9UEZHb28Xg/exec',
+  appsScriptWebAppUrl: 'https://script.google.com/macros/s/AKfycbwoImwAYVumgOzXh-GvmFxwVNIO5zlpb_BupD7JLimaWXnuW0pcAEk4WVx5E4MXvC-kYA/exec',
   requestTimeoutMs: 90000
 };
